4be9cb59000eef90509f8ade59d64a834a2ff1ec
jinguanchiou <158754679+jinguanchiou@users.noreply.github.com>
2024-04-26 12:44:50 +0800

    4/26
    寫了一段里爾的程式

---
+++ Assets/Script/Riru3DController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Riru3DController : MonoBehaviour
+{
+    public Transform player;
+    private Animator RiruAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RiruAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(Vector3.up, direction);
+    }
+    public void Walk(bool Walk)
+    {
+        RiruAnim.SetBool("Walk", Walk);
+    }
+    public void Enchant()
+    {
+        RiruAnim.SetTrigger("Enchant");
+    }
+    public void ThrustAttack()
+    {
+        RiruAnim.SetTrigger("ThrustAttack");
+    }
+    public void JunpAttack()
+    {
+        RiruAnim.SetTrigger("JunpAttack");
+    }
+    public void DoubleAttack()
+    {
+        RiruAnim.SetTrigger("DoubleAttack");
+    }
+}

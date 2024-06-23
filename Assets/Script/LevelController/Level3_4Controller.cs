4b8a5baa66154eec467de97651c6cecb8efab719
Damax40333 <167753295+Damax40333@users.noreply.github.com>
2024-06-23 17:52:35 +0800

    止損點0.1

---
+++ Assets/Script/LevelController/Level3_4Controller.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Level3_4Controller : MonoBehaviour
+{
+    public ConversationFunction Level3_4;
+    public GameObject DialogBox;
+    public GameObject Avatar;
+    public Text DialogBoxText;
+    public Transform PlayerTreanform;
+    public Transform PlayerAvatar;
+    public Transform OtherAvatar;
+
+    private GameObject Image_Log;
+    private PlayerController playerController;
+    private bool StartConversation = false;
+    private int currentTextIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        if (Level3_4.LevelAlreadyTold)
+        {
+            playerController.OnlockPlayer = false;
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Level3_4.LevelAlreadyTold && StartConversation)
+        {
+            if (currentTextIndex == 0)
+            {
+                playerController.IsConversation();
+                DialogBoxText.text = Level3_4.LevelTextMesh[0];
+                DialogBox.SetActive(true);
+                GenerateImage();
+                currentTextIndex++;
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && currentTextIndex < Level3_4.LevelTextMesh.Count)
+            {
+                Destroy(Image_Log);
+                DialogBoxText.text = Level3_4.LevelTextMesh[currentTextIndex];
+                GenerateImage();
+                currentTextIndex++;
+            }
+            if (currentTextIndex >= Level3_4.LevelTextMesh.Count)
+            {
+                DialogBox.SetActive(false);
+                Level3_4.LevelAlreadyTold = true;
+            }
+        }
+        if (Level3_4.LevelAlreadyTold)
+        {
+            playerController.OnlockPlayer = false;
+            Destroy(gameObject);
+        }
+    }
+    void GenerateImage()
+    {
+        if (Level3_4.avatar[currentTextIndex] != null)
+        {
+            if (Level3_4.avatar[currentTextIndex].name == "PlayerAvatar")
+            { Image_Log = Instantiate(Level3_4.avatar[currentTextIndex], PlayerAvatar); }
+            else
+            { Image_Log = Instantiate(Level3_4.avatar[currentTextIndex], OtherAvatar); }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartConversation = true;
+        }
+    }
+}
---
+++ Assets/Script/LevelController/Level3_8Controller.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Level3_8Controller : MonoBehaviour
+{
+    public ConversationFunction Level3_8;
+    public GameObject DialogBox;
+    public GameObject Avatar;
+    public Text DialogBoxText;
+    public Transform PlayerTreanform;
+    public Transform PlayerAvatar;
+    public Transform OtherAvatar;
+
+    private GameObject Image_Log;
+    private PlayerController playerController;
+    private bool StartConversation = false;
+    private int currentTextIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        if (Level3_8.LevelAlreadyTold)
+        {
+            playerController.OnlockPlayer = false;
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Level3_8.LevelAlreadyTold && StartConversation)
+        {
+            if (currentTextIndex == 0)
+            {
+                playerController.IsConversation();
+                DialogBoxText.text = Level3_8.LevelTextMesh[0];
+                DialogBox.SetActive(true);
+                GenerateImage();
+                currentTextIndex++;
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && currentTextIndex < Level3_8.LevelTextMesh.Count)
+            {
+                Destroy(Image_Log);
+                DialogBoxText.text = Level3_8.LevelTextMesh[currentTextIndex];
+                GenerateImage();
+                currentTextIndex++;
+            }
+            if (currentTextIndex >= Level3_8.LevelTextMesh.Count)
+            {
+                DialogBox.SetActive(false);
+                Level3_8.LevelAlreadyTold = true;
+            }
+        }
+        if (Level3_8.LevelAlreadyTold)
+        {
+            playerController.OnlockPlayer = false;
+            Destroy(gameObject);
+        }
+    }
+    void GenerateImage()
+    {
+        if (Level3_8.avatar[currentTextIndex] != null)
+        {
+            if (Level3_8.avatar[currentTextIndex].name == "PlayerAvatar")
+            { Image_Log = Instantiate(Level3_8.avatar[currentTextIndex], PlayerAvatar); }
+            else
+            { Image_Log = Instantiate(Level3_8.avatar[currentTextIndex], OtherAvatar); }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartConversation = true;
+        }
+    }
+}
